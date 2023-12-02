92c25b6818a3e050893c9fd98367738a02d16dc7
staaronf <ajjstevens@gmail.com>
2023-12-02 05:50:52 -0800

    google map added, tweaked quiz buttons

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -15,18 +15,4 @@ $(".quiz-button").on("click", function() {
 });
 
 
-$(".quiz-button").on("click", function() {
-  const Correct = $(this).data("correct");
-
-  if (Correct) {
-      $("#answer").text("Yes Sir! There was 9.").css("color", "green");
-      $(this).css("background-color", "green").css("color", "white");
-  } else {
-      $("#answer").text("bruh, try again").css("color", "red");
-      $(this).css("background-color", "red").css("color", "white");
-  }
-
-  //disable buttons after click
-  $(".quiz-button").prop("disabled", true);
-});
 
