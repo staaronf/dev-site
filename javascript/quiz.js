e392d5134e0758b336d53114c1900b7c34d899af
staaronf <ajjstevens@gmail.com>
2023-12-02 04:17:22 -0800

    update quiz file, correcting misplaced code

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,15 +1,32 @@
-const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
 
-function toggleMode() {
-  body.classList.toggle('dark-mode');
+$(".quiz-button").on("click", function() {
+  const Correct = $(this).data("correct");
 
-  const modeMessage = body.classList.contains('dark-mode') ?
-    'Dark Mode' 
-    : "Light Mode"
+  if (Correct) {
+      $("#answer").text("Yes Sir! There was 9.").css("color", "green");
+      $(this).css("background-color", "green").css("color", "white");
+  } else {
+      $("#answer").text("bruh, try again").css("color", "red");
+      $(this).css("background-color", "red").css("color", "white");
+  }
 
-  modeStatus.innerText = "Currently in " + modeMessage;
-}
+  //disable buttons after click
+  $(".quiz-button").prop("disabled", true);
+});
 
-modeToggle.addEventListener('click', toggleMode);
+
+$(".quiz-button").on("click", function() {
+  const Correct = $(this).data("correct");
+
+  if (Correct) {
+      $("#answer").text("Yes Sir! There was 9.").css("color", "green");
+      $(this).css("background-color", "green").css("color", "white");
+  } else {
+      $("#answer").text("bruh, try again").css("color", "red");
+      $(this).css("background-color", "red").css("color", "white");
+  }
+
+  //disable buttons after click
+  $(".quiz-button").prop("disabled", true);
+});
+
